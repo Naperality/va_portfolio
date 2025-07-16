a3c01fc43fd4c1d7c95c15d7f6970c7fddf71888
Naperality <napolytan@gmail.com>
2025-07-16 13:21:34 +0800

    add: layout for redirection page

---
+++ src/app/thankyou_page/layout.tsx
@@ -0,0 +1,8 @@
+// src/app/thankyou_page/layout.tsx
+export default function ThankYouLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
